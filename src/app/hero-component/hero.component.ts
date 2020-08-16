d8641fce25cba10f65101c13053991f8e79d3d99
Jeniffer Carvalho <jeniffer.carvalho@cbyk.com.br>
2020-08-16 20:08:00 -0300

    added hero component

---
+++ src/app/hero-component/hero.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { TOKEN } from '../../token'
+
+@Component({
+  selector: 'hero-component',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
+})
+export class HeroComponent implements OnInit {
+
+  constructor(private http: HttpClient) {}
+
+  heros: []
+
+  private url = `https://superheroapi.com/api.php/${TOKEN}/search/flash`
+
+  ngOnInit() {
+    this.http.get(this.url)
+            // .subscribe(res => console.log(res.results))
+            .subscribe(res => this.heros = res.results)
+  }
+}
